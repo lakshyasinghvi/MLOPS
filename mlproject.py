09f1b8f94ac37bc0116e0e50ce93f254e722c312
lakshyasinghvi <33891143+lakshyasinghvi@users.noreply.github.com>
2020-05-25 21:42:01 +0530

    Update mlproject.py

--- mlproject.py
+++ mlproject.py
@@ -8,33 +8,69 @@ from keras.datasets import mnist
 from keras.utils import np_utils
 import keras
 import sys
+import struct
+import numpy as np
+from keras import backend as K
+
+def read_idx(filename):
+    """Credit: https://gist.github.com/tylerneylon"""
+    with open(filename, 'rb') as f:
+        zero, data_type, dims = struct.unpack('>HBB', f.read(4))
+        shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
+        return np.frombuffer(f.read(), dtype=np.uint8).reshape(shape)
 
 sys.stdin=open('/root/mlops/input.txt','r')
+#After downloading our dataset we see it's coded in the ubyte form
+#We then use the following function to read the data and return it as a numpy array
+#We use the function to extact our training and test datasets
+x_train = read_idx("./fashion_mnist/train-images-idx3-ubyte")
+y_train = read_idx("./fashion_mnist/train-labels-idx1-ubyte")
+x_test = read_idx("./fashion_mnist/t10k-images-idx3-ubyte")
+y_test = read_idx("./fashion_mnist/t10k-labels-idx1-ubyte")
 
-(x_train, y_train), (x_test, y_test)  = mnist.load_data()
+#Let's create our model
 
+# Training Parameters
+batch_size = 128
+epochs = 3
+
+# Lets store the number of rows and columns
 img_rows = x_train[0].shape[0]
 img_cols = x_train[1].shape[0]
 
+# Getting our date in the right 'shape' needed for Keras
+# We need to add a 4th dimenion to our date thereby changing our
+# Our original image shape of (60000,28,28) to (60000,28,28,1)
 x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
 x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
 
+# store the shape of a single image 
 input_shape = (img_rows, img_cols, 1)
 
+# change our image type to float32 data type
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 
+# Normalize our data by changing the range from (0 to 255) to (0 to 1)
 x_train /= 255
 x_test /= 255
 
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# Now we one hot encode outputs
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
+
+# Let's count the number columns in our hot encoded matrix 
+print ("Number of Classes: " + str(y_test.shape[1]))
 
 num_classes = y_test.shape[1]
 num_pixels = x_train.shape[1] * x_train.shape[2]
 
+# create model
 model = Sequential()
-
 convlayers = int(input())
 first_layer_nfilter = int(input())
 first_layer_filter_size = int(input())
@@ -92,7 +128,7 @@ history = model.fit(x_train, y_train,
           validation_data=(x_test, y_test),
           shuffle=True)
 
-model.save("mlopsprj.h5")
+model.save("mlopsproject.h5")
 
 scores = model.evaluate(x_test, y_test, verbose=1)
 print('Test loss:', scores[0])
