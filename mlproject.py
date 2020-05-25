ceee22206c800faa194a764bca6f6f4ee0769ca9
lakshyasinghvi <33891143+lakshyasinghvi@users.noreply.github.com>
2020-05-25 20:45:40 +0530

    Update mlproject.py

--- mlproject.py
+++ mlproject.py
@@ -9,7 +9,7 @@ from keras.utils import np_utils
 import keras
 import sys
 
-sys.stdin=open('/mlops/input.txt','r')
+sys.stdin=open('/root/mlops/input.txt','r')
 
 (x_train, y_train), (x_test, y_test)  = mnist.load_data()
 
